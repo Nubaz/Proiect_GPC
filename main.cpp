ea7bf1e6c23cd26f063fd8659a7539de68d740f1
nubaz <lucapaulf@gmail.com>
2023-04-24 18:17:54 +0300

    Modificat masina si gropi

--- main.cpp
+++ main.cpp
@@ -6,22 +6,22 @@
 */
 #include <windows.h>
 #include <math.h>
+#include <time.h>
 #include <stdlib.h>
 #include <GL/freeglut.h>
 #include <iostream>
 
 using namespace std;
 
-double idxGropi = 0;
-int hardLeft = 0, hardRight = 0;
+double idxGropi = 0.;
+double viteza = 2.;
+double fazaFar = 1.;
+double sirena = 0.5;
 
-const double TWO_PI = 6.2831853;
-double i = 0.;
+double xcar = 0., ycar = 0.;
 // unde se va opri masina pe axa x, cand face schimbarea de benzi
 double xFinal = 100;
 double j = 1.0;
-double k = 0;
-double vitezaTrafic = 0.15;
 GLsizei winWidth = 500, winHeight = 500;
 GLuint traficID;
 static GLfloat rotTheta = 0.0;
@@ -51,15 +51,14 @@ class scrPt {
 		}
 };
 
-
-void miscareGirofar(void) {
-	// pentru girofar
+void funcTranzitii(void) {
+	// Girofar
 	j += 10;
 
-	// pentru gropi
-	idxGropi -= 0.85;
-	if (idxGropi == -500) {
-		idxGropi += 0.85;
+	// Gropi
+	idxGropi -= viteza;
+	if (idxGropi < -500.) {
+		idxGropi = 500.;
 	}
 
 	glutPostRedisplay();
@@ -75,10 +74,7 @@ static void init(void) {
 	glClearColor(0.6235294, 0.239215, 0, 1);
 	gluOrtho2D(-100, 100, -100, 100);
 
-
 	traficID = glGenLists(1);
-
-
 
 	for (aux = 0; aux < 20; aux++)
 	{
@@ -90,48 +86,60 @@ static void init(void) {
 	}
 	glEndList();
 
-	// pentru opacitatea culorilor
+	// Opacitatea culorilor
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
 }
 
-void mouse(int button, int state, int x, int y) {
-	switch (button) {
-		case GLUT_LEFT_BUTTON:
-			if (state == GLUT_DOWN)
-				i -= 1;
+void keyboard(unsigned char key, int x, int y) {
+	switch (key) {
+		case 'w':
+			ycar += 5;
 			break;
-		case GLUT_RIGHT_BUTTON:
-			if (state == GLUT_DOWN)		
-				i += 1;
+		case 'a':
+			xcar -= 5;
+			if (xcar < -220) xcar = -220;
 			break;
+		case 's':
+			ycar -= 5;
+			break;
+		case 'd':
+			xcar += 5;
+			if (xcar > 120) xcar = 120;
+			break;
+		case 'f':
+			if (fazaFar - 0.2 <= 0.) fazaFar = -1.;
+			else fazaFar -= 0.2;
+			if (fazaFar == -1.) fazaFar = 1.;
+			break;
+		case 'g':
+			if (sirena == 0.5) sirena = 0.;
+			else sirena = 0.5;
 		default:
 			break;
 	}
 }
 
-void groapa(int origx, int origy) {
+void genGroapa(int origx, int origy) {
+	int range = 100, min = 0, nrVf = rand() % 10 + 4;
+
 	glColor3f(0.2, 0.2, 0.2);
 	glBegin(GL_POLYGON);
-		glVertex2i(0 + origx, 0 + origy);
-		glVertex2i(40 + origx, -20 + origy);
-		glVertex2i(60 + origx, 0 + origy);
-		glVertex2i(60 + origx, 60 + origy);
-		glVertex2i(20 + origx, 40 + origy);
-		glVertex2i(0 + origx, 60 + origy);
+		for (int iter = 0; iter < nrVf; iter++) {
+			glVertex2i(rand() % range + min + origx, rand() % range + min + origy);
+		}
 	glEnd();
-
 }
 
 void desenDrum(void) {
+	srand(2);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 	glMatrixMode(GL_MODELVIEW);
-
 	glLoadIdentity();
 
-	// DRUMUL
+	// ----- DRUMUL -----
 	glColor3f(0.3, 0.3, 0.3);
 	glRectf(-200., -1000., 200., 1000.);
 
@@ -142,14 +150,15 @@ void desenDrum(void) {
 	// Gropi
 	glPushMatrix();
 	glTranslated(0, idxGropi, 0);
-	groapa(20, 20);
+	genGroapa(20, 20);
+	genGroapa(-200, 100);
 
-	// initializare pentru tranzitii
+	// Initializare pentru tranzitii
 	glPopMatrix();
-	glTranslated(i, 0, 0);
+	glTranslated(xcar, ycar, 0);
 
-	// MASINA DE POLITIE
-	// sasiu
+	// ----- MASINA DE POLITIE -----
+	// Sasiu
 	glColor3f(0, 0, 0);
 	glRectf(20, -180, 80, -155);
 	glColor3f(1, 1, 1);
@@ -157,59 +166,56 @@ void desenDrum(void) {
 	glColor3f(0, 0, 0);
 	glRectf(20, -115, 80, -90);
 
-	// stopuri
+	// Stopuri
 	glColor3f(1, 0, 0);
 	glRectf(25, -185, 35, -180);
 	glColor3f(1, 0, 0);
 	glRectf(65, -185, 75, -180);
 
-	// faruri
+	// Faruri
 	glColor3f(0.8, 0.5, 0);
 	glRectf(25, -90, 35, -85);
 	glColor3f(0.8, 0.5, 0);
 	glRectf(65, -90, 75, -85);
 
-	glColor3f(1, 1, 0);
+	glColor4f(1, 1, 0, fazaFar);
 	glBegin(GL_TRIANGLES);
-	glVertex2f(30, -85);
-	glVertex2f(0, 0);
-	glVertex2f(60, 0);
+		glVertex2f(30, -85);
+		glVertex2f(0, 0);
+		glVertex2f(60, 0);
 
-	glVertex2f(40, 0);
-	glVertex2f(100, 0);
-	glVertex2f(70, -85);
-
+		glVertex2f(40, 0);
+		glVertex2f(100, 0);
+		glVertex2f(70, -85);
 	glEnd();
 
 
-	// GIROFAR
-
-
+	// ----- GIROFAR -----
+	// Animatie
 	glTranslated(50, -135, 0);
 	glRotated(j, 0, 0, 1);
 	glTranslated(-50, 135, 0);
 
-	//girofar
-	glColor4f(1, 0, 0, 0.5);
+	// Girofarul efectiv
+	glColor4f(1, 0, 0, sirena);
 	glBegin(GL_TRIANGLES);
-	glVertex2f(10, -115);
-	glVertex2f(50, -135);
-	glVertex2f(10, -155);
+		glVertex2f(10, -115);
+		glVertex2f(50, -135);
+		glVertex2f(10, -155);
 	glEnd();
 
-
-	glColor4f(0, 0, 1, 0.5);
+	glColor4f(0, 0, 1, sirena);
 	glBegin(GL_TRIANGLES);
-	glVertex2f(50, -135);
-	glVertex2f(90, -115);
-	glVertex2f(90, -155);
+		glVertex2f(50, -135);
+		glVertex2f(90, -115);
+		glVertex2f(90, -155);
 	glEnd();
 
 	glPopMatrix();
-
 	glPopMatrix();
-	// incrementare pentru girofar si alte translatii
-	glutIdleFunc(miscareGirofar);
+
+	// Incrementare pentru girofar si alte translatii
+	glutIdleFunc(funcTranzitii);
 
 	glutSwapBuffers();
 	glFlush();
@@ -239,9 +245,7 @@ void main(int argc, char** argv) {
 
 	glutDisplayFunc(desenDrum);
 	glutReshapeFunc(winReshapeFcn);
-
-	glutMouseFunc(mouse);
-	glutAttachMenu(GLUT_MIDDLE_BUTTON);
+	glutKeyboardFunc(keyboard);
 
 	glutMainLoop();
 }
